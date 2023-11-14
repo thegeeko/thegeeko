eb65b8793e9ff87dc78aa7d2421d7ae0e63b12a7
Abdelhadi <abdelhadims@icloud.com>
2023-11-14 04:18:13 +0200

    site url fix

--- src/config.ts
+++ src/config.ts
@@ -8,5 +8,4 @@ export const TWITTER_HANDLE = "the_geeko1";
 export const MY_NAME = "Abdelhadi";
 
 // setup in astro.config.mjs
-const BASE_URL = new URL(import.meta.env.SITE);
-export const SITE_URL = BASE_URL.origin;
+export const SITE_URL = "https://thegeeko.me"
